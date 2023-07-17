49391af941a9c88178b3e84419820619d4749ae8
Dipen Chauhan <get.dipen@gmail.com>
2023-07-16 20:32:47 -0700

    displaying login button

--- src/components/UnauthenticatedApp/index.jsx
+++ src/components/UnauthenticatedApp/index.jsx
@@ -1,5 +1,19 @@
+import { useAuth } from "../../hooks/useAuth";
+import "./styles.css";
+
 function UnauthenticatedApp() {
-  return <div>I'm unauthenticated!</div>;
+  const { login } = useAuth();
+
+  return (
+    <>
+      <h2>Log in to join a chat room!</h2>
+      <div>
+        <button onClick={login} className="login">
+          Login with Google
+        </button>
+      </div>
+    </>
+  );
 }
 
 export { UnauthenticatedApp };
