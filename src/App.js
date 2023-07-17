530fe9bf3dbd030339e93d5d62c0f055edc078ce
Dipen Chauhan <get.dipen@gmail.com>
2023-07-16 20:24:22 -0700

    displaying chatroom container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { AuthenticatedApp } from "./components/AuthenticatedApp";
+import { UnauthenticatedApp } from "./components/UnauthenticatedApp";
+import { useAuth } from "./hooks/useAuth";
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const { user } = useAuth();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>💬 Chat Room</h1>
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
