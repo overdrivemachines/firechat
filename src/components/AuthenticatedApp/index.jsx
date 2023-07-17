8b167d84eb48eb2c0d1a7f558561f5d3adc143d2
Dipen Chauhan <get.dipen@gmail.com>
2023-07-16 20:45:43 -0700

    rendering router

--- src/components/AuthenticatedApp/index.jsx
+++ src/components/AuthenticatedApp/index.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from "../Landing";
+import { ChatRoom } from "../ChatRoom";
+
+// <AuthenticatedApp> will render a router that contains two routes:
+// one with path / that takes us to a <Landing> component,
+// and another with the path /room/:id that renders a <ChatRoom> component.
 function AuthenticatedApp() {
-  return <div>I'm authenticated!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/room/:id" element={<ChatRoom />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export { AuthenticatedApp };
